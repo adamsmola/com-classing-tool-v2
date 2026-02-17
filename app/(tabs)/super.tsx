f308b390f849dcc13292e0a833b66bed94cad910
Adam Smola <asmola@Mac.localdomain>
2026-02-17 18:21:46 -0500

    Revert "Refactor to seperate business logic for super class and add some unit tests"
    This reverts commit dd3d53681c96541f886ce79fa820bc43cad1ed31.

--- app/(tabs)/super.tsx
+++ app/(tabs)/super.tsx
@@ -1,13 +1,45 @@
+
 import { useState } from "react";
-import { Picker, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-import {
-  SUPER_CLASSES,
-  getClassByPowerToWeight,
-  getCorrectedPower,
-  getMaxPowerToWeightByClass,
-  getMinPowerToWeightByClass,
-  getPowerToWeight,
-} from "../../lib/superClass";
+import { Picker, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const SUPER_CLASS_LIMITS = [
+  { name: "Super Unlimited", minPtw: 0, maxPtw: 5.999 },
+  { name: "Super A", minPtw: 6, maxPtw: 8.799 },
+  { name: "Super B", minPtw: 8.8, maxPtw: 12.399 },
+  { name: "Super C", minPtw: 12.4, maxPtw: 15.799 },
+  { name: "Super D", minPtw: 15.8, maxPtw: 18.999 },
+  { name: "Super E", minPtw: 19, maxPtw: 99 },
+] as const;
+
+const SUPER_CLASSES = SUPER_CLASS_LIMITS.map((c) => c.name);
+
+function getClassByPowerToWeight(power_to_weight: number): string {
+  const found = SUPER_CLASS_LIMITS.find((c) => power_to_weight < c.maxPtw);
+  return found ? found.name : SUPER_CLASS_LIMITS[SUPER_CLASS_LIMITS.length - 1].name;
+}
+
+function getMinPowerToWeightByClass(sclass: string): number {
+  return SUPER_CLASS_LIMITS.find((c) => c.name === sclass)?.minPtw ?? 0;
+}
+
+function getMaxPowerToWeightByClass(sclass: string): number {
+  return SUPER_CLASS_LIMITS.find((c) => c.name === sclass)?.maxPtw ?? 99;
+}
+
+function getCorrectedPower(horsepower: number, torque: number, dynoType: string): number {
+  const correctionFactors: Record<string, number> = {
+    "AWD": 0.93,
+    "2WD": 0.91,
+    "2WD-100": 1,
+  };
+
+  const corrFactor = correctionFactors[dynoType] ?? 1;
+  return ((2 / 3) * horsepower + (1 / 3) * torque) * corrFactor;
+}
+
+function getPowerToWeight(weight: number, corrected_power: number): number {
+  return weight / corrected_power;
+}
 
 export default function SuperScreen() {
   const [horsepower, setHorsepower] = useState('');
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/lib/**/*.test.js"],
-  transform: {
-    "\\.js$": "babel-jest",
-  },
-  transformIgnorePatterns: ["/node_modules/(?!(@react-native|expo)/)"],
-};
